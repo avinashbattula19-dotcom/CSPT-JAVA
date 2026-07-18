b666eb1ca17319b70edb0a44e44966929c07f658
avinashbattula19-dotcom <avinashbattula19@gmail.com>
2026-07-18 13:47:01 +0000

    prog2.java

---
+++ prog2.java
@@ -0,0 +1,7 @@
+public class prog2 {
+    public static void main(String args[]){
+        System.out.println("MAX value"+Integer.MAX_VALUE);
+        System.out.println("MIN VALUE"+Integer.MIN_VALUE);
+    }
+    
+}
